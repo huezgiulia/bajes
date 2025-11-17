7b8695649891844093cbec598d3e8360b4a3af2f
ghuez <giulia.huez@uni-jena.de>
2025-11-17 10:43:40 +0100

    vel dynamics lim

--- bajes/pipe/log_like.py
+++ bajes/pipe/log_like.py
@@ -275,10 +275,9 @@ class KNLikelihood(Likelihood):
     def log_like(self, params):
         
         # check the dynamical velocity  parameters value (for the case with NR fit)
-        if 'Xkn' in self.approx:
-            if params['vel_dynamics'] > 0.333 or params['vel_dynamics'] < 1e-4:
-                logL = -np.inf
-                return logL
+        if params['vel_dynamics'] > 0.333 or params['vel_dynamics'] < 1e-4:
+            logL = -np.inf
+            return logL
         
         if '3-NRfits' in self.approx:
             # check the two disk_frac parameters values (for the case with NR fit)
@@ -332,7 +331,6 @@ class KNLikelihood(Likelihood):
                 residuals   = ((self.filters.magnitudes[bi]-interp_mag)/self.filters.mag_stdev[bi])**2.
                 logL       += -0.5*residuals.sum() 
             logL += self.logNorm
-
         return logL
 
 
