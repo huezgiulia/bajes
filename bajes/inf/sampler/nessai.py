8bf6746f61ec66755f1423616ed4e1db1dfa0260
ghuez <giulia.huez@uni-jena.de>
2026-06-01 17:22:31 +0200

    MMA proposal

--- bajes/inf/sampler/nessai.py
+++ bajes/inf/sampler/nessai.py
@@ -8,12 +8,95 @@ logger = logging.getLogger(__name__)
 import nessai
 from nessai.model import Model
 from nessai.flowsampler import FlowSampler
-from nessai.gw.proposal import GWFlowProposal
+# from nessai.gw.proposal import GWFlowProposal
+from nessai.proposal import FlowProposal
+from nessai.gw.reparameterisations import get_gw_reparameterisation
 from nessai.utils.logging import configure_logger
 
 from . import SamplerBody
 
 import os
+
+class GWFlowProposalBajes(FlowProposal):
+    """Wrapper for FlowProposal that has defaults for CBC-PE
+        From nessai code, adapted for bajes MMA"""
+
+    aliases = {
+        "chirp_mass": ("mass", None),
+        "mass_ratio": ("mass_ratio", None),
+        # "ra": ("sky-ra-dec", ["dec", "Dec"]),
+        # "dec": ("sky-ra-dec", ["ra"]),
+        "azimuth": ("sky-az-zen", ["zenith", "zen", "Zen", "Zenith"]),
+        "zenith": ("sky-az-zen", ["azimuth", "az", "Az", "Azimuth"]),
+        "theta_1": ("angle-sine", None),
+        "theta_2": ("angle-sine", None),
+        "tilt_1": ("angle-sine", None),
+        "tilt_2": ("angle-sine", None),
+        "theta_jn": ("angle-sine", None),
+        "iota": ("angle-sine", None),
+        "phi_jl": ("angle-2pi", None),
+        "phi_12": ("angle-2pi", None),
+        "phase": ("angle-2pi", None),
+        "psi": ("angle-pi", None),
+        "geocent_time": ("time", None),
+        "time_jitter": ("periodic", None),
+        "a_1": ("default", None),
+        "a_2": ("default", None),
+        "chi_1": ("default", None),
+        "chi_2": ("default", None),
+        "luminosity_distance": ("distance", None),
+    }
+    """
+    Dictionary of aliases used to determine the default reparameterisations
+    for common gravitational-wave parameters.
+    """
+    use_default_reparameterisations = True
+    """
+    GW specific reparameterisations will be used by default. This is different
+    to the parent class where they are disabled by default.
+    """
+
+    def get_reparameterisation(self, reparameterisation):
+        """Function to get reparameterisations that checks GW defaults and
+        aliases
+        """
+        return get_gw_reparameterisation(reparameterisation)
+
+    def add_default_reparameterisations(self):
+        """
+        Add default reparameterisations for parameters that have not been
+        specified.
+        """
+        parameters = [
+            n
+            for n in self.model.names
+            if n not in self._reparameterisation.parameters
+        ]
+        logger.info(f"Adding default reparameterisations for {parameters}")
+
+        for p in parameters:
+            logger.debug(f"Trying to add reparameterisation for {p}")
+            if p in self._reparameterisation.parameters:
+                logger.debug(f"Parameter {p} is already included")
+                continue
+            name, extra_params = self.aliases.get(p.lower(), (None, None))
+            if name is None:
+                logger.debug(f"{p} is not a known GW parameter")
+                continue
+            if extra_params is not None:
+                p = [p] + [ep for ep in extra_params if ep in self.model.names]
+            else:
+                p = [p]
+            prior_bounds = {k: self.model.bounds[k] for k in p}
+            reparam, kwargs = get_gw_reparameterisation(name)
+            logger.info(
+                f"Adding reparameterisation {reparam.__name__} for {p} "
+                f"with config: {kwargs}"
+            )
+            self._reparameterisation.add_reparameterisation(
+                reparam(parameters=p, prior_bounds=prior_bounds, **kwargs)
+            )
+
 
 
 class NessaiModel(Model):
@@ -59,7 +142,7 @@ class SamplerNessai(SamplerBody):
         configure_logger(output=self.outdir,label='bajes')
         self.sampler = FlowSampler(NessaiModel(posterior), output=self.outdir, nlive=kwargs['nlive'], 
                                    stopping=kwargs['tolerance'], n_pool=pool._processes,
-                                   flow_class=GWFlowProposal, analytic_priors=True,
+                                   flow_class=GWFlowProposalBajes, analytic_priors=True,
                                    flow_config=dict(n_blocks=6,n_neurons=40),
                                    )
 
